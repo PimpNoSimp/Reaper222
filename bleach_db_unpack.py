b5d5c7f17ae9e08469e992e515d7923a59eaa5cf
PimpNoSimp <126896656+PimpNoSimp@users.noreply.github.com>
2023-03-03 09:53:46 -0600

    Add files via upload

---
+++ bleach_db_unpack.py
@@ -0,0 +1,36 @@
+
+# Requires python3 and the py3rijndael package:
+#	>> py -3 -m pip install py3rijndael
+from py3rijndael import Rijndael
+import sys
+import io
+import gzip
+
+if len(sys.argv) < 3:
+	print("Usage: {} <input file> <output file> [OPTIONAL <key>]".format(sys.argv[0]))
+else:
+	in_file_name = sys.argv[1]
+	out_file_name = sys.argv[2]
+
+# Optional key argument with default value.
+key = b'1479f22faea6ad4630c1d7fdc6851509'
+if len(sys.argv) == 4:
+	key = sys.argv[3]
+
+
+with open(in_file_name, 'rb') as in_file:
+	with open(out_file_name, 'wb') as out_file:
+		# Do the decryption. This will take a minute, doing this 32 bytes at a time (256bit block size) with py3rijndael is _slow_.
+		print('Decrypting...')
+		cipher = Rijndael(key, block_size=32)
+		output = io.BytesIO()
+		while True:
+			data = in_file.read(32);
+			if len(data) == 0:
+				break
+			output.write(cipher.decrypt(data))
+
+		# Decompress glib
+		print('Decompressing...')
+		decomp = gzip.decompress(output.getvalue()) # 1F 8B 08 gzip header
+		out_file.write(decomp)
